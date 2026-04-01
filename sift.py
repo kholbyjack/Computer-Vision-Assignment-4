7791d32457b6d8191d4c44e2f31e3806b68979db
kholbyjack <kholbert.2015@gmail.com>
2026-04-01 14:53:27 -0400

    Literally nothing done hahah

---
+++ sift.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+
+# pinpoint locations for finding features
+'''
+https://docs.opencv.org/4.x/da/df5/tutorial_py_sift_intro.html
+Steps:
+    - find the difference of gaussian
+    - local extrema are potential key points
+'''
+def scale_space ():
+    print("ajsdf")
+
+# find key points
+
+# assign orientation to points
+
+# describe key points
+
